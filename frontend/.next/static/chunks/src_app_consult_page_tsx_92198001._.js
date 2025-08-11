f1f0acdaabe1ce23e56dddd912f27ffee5a29813
AdhyayanDubey <ad0963@srmist.edu.in>
2025-08-11 12:04:54 +0530

    2nd

--- frontend/.next/static/chunks/src_app_consult_page_tsx_92198001._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_eb871211._.js",
-  "static/chunks/node_modules_fac49550._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/src_app_page_tsx_92198001._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_54f5d51d._.js",
-  "static/chunks/node_modules_23268b52._.js"
-],
-    source: "dynamic"
-});
